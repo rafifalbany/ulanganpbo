bb89d659118ecda1414356d97b367939867ad2ba
rafifalbany <71073945+rafifalbany@users.noreply.github.com>
2020-09-10 17:49:41 +0700

    Add files via upload

---
+++ UTS.java
@@ -0,0 +1,20 @@
+package Ujian;
+
+public class UTS {
+    public static void main(String[] args) {
+        String chara = "ABCDEFGHIJKLMNOPQRSTUVWXYZ-# ";
+        char word1 = chara.charAt(17);
+        char word2 = chara.charAt(0);
+        char word3 = chara.charAt(5);
+        char word4 = chara.charAt(8);
+        char word5 = chara.charAt(3);
+        char word6 = chara.charAt(28);
+        char word7 = chara.charAt(11);
+        char word8 = chara.charAt(26);
+        char word9 = chara.charAt(1);
+        char word10 = chara.charAt(13);
+        
+    
+        System.out.println("" +  word1 + word2 + word3 + word4  + word3 + word6 + word2+ word7+ word8+ word9+ word2+ word10 + word4);
+    }
+}
